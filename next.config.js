f21fd489b6a5a2712dc55aca2e0824e657232961
Lykosis2 <154882230+Lykosis2@users.noreply.github.com>
2025-08-16 15:04:25 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,5 @@
 const nextConfig = {
   reactStrictMode: false,
   output: 'standalone',
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      // @ = proje kökü (path değişkeni tanımlamadan çözüyoruz)
-      '@': require('path').resolve(__dirname),
-    };
-    return config;
-  },
 };
-
 module.exports = nextConfig;
